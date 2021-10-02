e39a73ba4946f856b3b61dbe88983f7ce630a823
Charles <c.martinelli@rubika-edu.com>
2021-10-03 00:51:38 +0200

    package menu + scripts pause et music managers

---
+++ FarmJam/Assets/CHM_Scripts/MusicManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length>1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
