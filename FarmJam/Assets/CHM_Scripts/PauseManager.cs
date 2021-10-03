cac858a6454c39f32acd1274c734f3d64d04a560
Charles <c.martinelli@rubika-edu.com>
2021-10-03 02:30:00 +0200

    Ajout sons et scripting :(

--- FarmJam/Assets/CHM_Scripts/PauseManager.cs
+++ FarmJam/Assets/CHM_Scripts/PauseManager.cs
@@ -9,16 +9,18 @@ public class PauseManager : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown("pause"))
+        if (Input.GetButtonDown("Pause") && Time.timeScale != 0)
         {
-            if (Time.timeScale == 1)
-            {
-                Time.timeScale = 0;
-                Debug.Log("timeScale is now 0");
-            }
-            else
-                Time.timeScale = 1;
-            Debug.Log("timeScale is now 1");
+
+            Time.timeScale = 0;
+            Debug.LogError("timeScale is now 0");
         }
+        
+        else if (Input.GetButtonDown("Pause") && Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            Debug.LogError("timeScale is now 1");
+        }
+        
     }
 }
